1a04adc7f846245403e15c8955ec9ef8e1e74296
akhil <akhilmulagada20@gmail.com>
2025-04-27 03:48:34 +0530

    removed unsed stuff

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,14 @@
-import Navbar from '../components/landing/Navbar';
+
 import HeroSection from "../components/landing/HeroSection";
 import FeatureSection from "../components/landing/FeatureSection";
 import Workflow from "../components/landing/Workflow";
-import Footer from "../components/landing/Footer";
-import Pricing from "../components/landing/Pricing";
-import Testimonials from "../components/landing/Testimonials";
+
 
 
 
 const Home = () => {
     return (
       <>
-        {/* <Navbar /> */}
-        {/* <div className="max-w-7xl mx-auto pt-20 px-6"> */}
         <div >
           <HeroSection />
           <FeatureSection />
