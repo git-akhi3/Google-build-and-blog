898da1141db6ebd4cf95e43c5dfaba8ac5c2e02a
akhil <akhilmulagada20@gmail.com>
2025-04-27 16:29:08 +0530

    added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ChatInterface from './pages/ChatInterface';
 import Home from './pages/Home';
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/chat" element={<ChatInterface />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat" element={<ChatInterface />} />
+        </Routes>
+      </Router>
+      <Analytics />
+    </>
   );
 };
 
